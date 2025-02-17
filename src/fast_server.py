e3b94ea17f9ae589ed842099c814253d068ad27b
brannentaylor-ccm <brannen.taylor@ccm.com>
2025-02-17 11:01:26 +0000

    initial

---
+++ src/fast_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""Containerizing a fast api application.
+Watching youtube Neural9 video.
+https://youtu.be/0TFWtfFY87U?t=791
+Brannen Taylor - 20240217
+
+"""
+
+def main():
+    """Main is a runner function, used to call individual functions in order."""
+    pass
+
+
+############### START HERE #####################
+# if__name__ - will look into a hidden variable for the file, called 'dunder name', or 'double underscore name'.
+# if this script is being run directly, as a script, and not just a container of functions, the
+# if__name__ will run, to inspect the name of the script, which will be '__main__', and will evaluate to True.
+# if __name__ is True, then go set some settings, and then go run the 'main()' function.
+################################################
+if __name__ == "__main__":
+    main()
